f7cc01f32d2776e569e4e3d1fa4eb8e11e3a7e35
shopnobaz <vision2025bd@yahoo.com>
2022-09-09 23:39:20 +0200

    set on delete/on update null

--- schedule-data-service/index.js
+++ schedule-data-service/index.js
@@ -47,7 +47,7 @@ server.use('/assets', express.static('../admin/dist/assets'))
 require('./routes/teachers.js')(server, db)
 require('./routes/login.js')(server, db)
 require('./routes/schools.js')(server, db)
-
+require('./routes/classes.js')(server, db)
 
 const apiDescription = require('./api-description.js')(host)
 
