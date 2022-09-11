c46086574a9f4fda14970c8757ff4daadb5304bf
shopnobaz <vision2025bd@yahoo.com>
2022-09-12 00:32:44 +0200

    cypress test folder edited

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
