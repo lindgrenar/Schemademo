39424dd822e8dae37eab22403c1a380d5155a1e3
lindgrenar <lindgren184@gmail.com>
2022-09-14 14:39:52 +0200

    Update courses.jsx
    Course(s)

--- admin/src/courses.jsx
+++ admin/src/courses.jsx
@@ -37,3 +37,23 @@ export const CoursesEdit = () => (
     </Datagrid>
   </Edit>
 );
+
+
+export const CoursesCreate = props => (
+  <Create {...props} >
+    <SimpleForm>
+      <TextField disabled source="id" reference="courses" />
+      <TextField source='name' />
+      <TextField source='shortName' />
+      <NumberField source='class' />
+      <NumberField source='points' />
+      <TextField source='startDate' />
+      <TextField source='endDate' />
+      <TextField source='plan' />
+      <NumberField source='invoiceItem' />
+      <NumberField source='hoursPerDay' />
+      <BooleanField source='hide' />
+      <EditButton />
+    </SimpleForm >
+  </Create >
+);
