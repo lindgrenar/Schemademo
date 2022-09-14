fa4d16649ebc96da5e445942f5f39b752e1cbd84
Sophex <philip.veljkovic@outlook.com>
2022-09-14 11:12:28 +0200

    installed cypress and added some tests

--- cypress/e2e/update-class.cy.js
+++ cypress/e2e/update-class.cy.js
@@ -1,5 +1,21 @@
-describe('The Login Page', () => {
-  it('Succsesfully loads', () => {
-    cy.visit('login.html')
+describe('The Login Page and Home Page', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('#email').type('exempel@nodehill.com')
+    cy.get('#password').type('abc123')
+    cy.get('[type="submit"]').click()
   })
+
+  it('Is logged in', () => {
+    cy.url().should('eq', 'http://localhost:7666/')
+  })
+
+  it('visits the admin dashboard', () => {
+    cy.visit('/admin')
+    cy.url().should('eq', 'http://localhost:7666/admin/')
+    cy.get('[href="#/classes"]').click()
+    cy.get(':nth-child(56) > .column-id').click()
+  })
+
 })
