4a4cec6c8302967804dd43f9e5b30a446fcdd1e0
Sophex <philip.veljkovic@outlook.com>
2022-09-16 12:19:18 +0200

    cypress test added to test creating a course

--- cypress/e2e/schedule-test/test_coursePage.cy.js
+++ cypress/e2e/schedule-test/test_coursePage.cy.js
@@ -5,16 +5,29 @@ const loging = new LoginPage()
 
 describe('Test login page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:7666/login.html')
+    cy.visit('/')
+    cy.get('#email').type('exempel@nodehill.com')
+    cy.get('#password').type('abc123')
+    cy.get('[type="submit"]').click()
   })
 
+  it('Is logged in', () => {
+    cy.url().should('eq', 'http://localhost:7666/')
+  })
 
-  it('Schedule proeject login function test', () => {
-    loging.enterUsername('exempel@nodehill.com')
-    loging.enterPassword('abc123')
-    loging.clickLogin()
-    cy.visit('http://localhost:7666/admin/#/courses')
-
+  it('visits the admin dashboard and creates a new course', () => {
+    cy.visit('/admin')
+    cy.url().should('eq', 'http://localhost:7666/admin/')
+    cy.get('[href="#/courses"]').click()
+    cy.get('.MuiToolbar-root > a.MuiButton-root').click({force: true})
+    cy.get('#name').type('TestCourse')
+    cy.get('#shortName').type('TestCourse')
+    cy.get('#class').type('32')
+    cy.get('#points').type('100')
+    cy.get('#plan').type('test')
+    cy.get('#invoiceItem').type('1')
+    cy.get('#hoursPerDay').type('4')
+    cy.get('.RaToolbar-defaultToolbar > .MuiButton-root').click()
   })
 
 })
